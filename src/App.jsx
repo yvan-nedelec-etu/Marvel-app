ed14c86c8dd36ef5382aab6bf2576aa8c598b17b
aaaa <yvan.nedelec@etu.univ-poitiers.fr>
2025-09-17 18:16:29 +0200

    Create CharactersList component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import characters from '../data/characters.json'
+import CharactersList from './components/CharactersList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,22 +11,13 @@ function App() {
   return (
     <>
       <div>
-      <a href="https://but-sd.github.io/prez/marvel.png" target="_blank" rel="noopener">
-        <img src="https://but-sd.github.io/prez/marvel.png" className="logo marvel" alt="Marvel logo" />
-      </a>
+        <a href="https://but-sd.github.io/prez/marvel.png" target="_blank" rel="noopener">
+          <img src="https://but-sd.github.io/prez/marvel.png" className="logo marvel" alt="Marvel logo" />
+        </a>
       </div>
       <h1>Marvel App</h1>
 
-      <ul id="characters">
-        {Array.isArray(characters) ? (
-          characters.map((c, i) => {
-            const label = typeof c === 'string' ? c : (c && (c.name || c.title || c.label)) || JSON.stringify(c)
-            return <li key={i}>{label}</li>
-          })
-        ) : (
-          <li>No characters</li>
-        )}
-      </ul>
+      <CharactersList characters={characters} />
     </>
   )
 }
