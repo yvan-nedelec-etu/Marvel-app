89b133a122d9f3ac4bd3da929bb012be0599c9c2
aaaa <yvan.nedelec@etu.univ-poitiers.fr>
2025-09-17 18:10:07 +0200

    Use static list for characters

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,18 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <a href="https://but-sd.github.io/prez/marvel.png" target="_blank" rel="noopener">
+        <img src="https://but-sd.github.io/prez/marvel.png" className="logo marvel" alt="Marvel logo" />
+      </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Marvel App</h1>
+
+      <ul id="characters">
+        <li>Beast</li>
+        <li>Captain America</li>
+        <li>Deadpool</li>
+      </ul>
     </>
   )
 }
-
 export default App
